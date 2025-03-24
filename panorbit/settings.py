d1fea45f08876827789a65693d58b9bef3d595fd
Prakhar Jain <78018165+Prakhar8003@users.noreply.github.com>
2025-03-24 12:07:27 +0530

    Update settings.py

--- panorbit/settings.py
+++ panorbit/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'world',
         'USER': 'root',
-        'PASSWORD': 'xxxx',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }   
